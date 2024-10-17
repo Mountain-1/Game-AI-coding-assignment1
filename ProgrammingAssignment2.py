6bd649f08812d076113781c9643c73373f3e9061
Ethan <erp0011@uah.edu>
2024-10-17 16:22:46 -0500

    commit3

--- ProgrammingAssignment2.py
+++ ProgrammingAssignment2.py
@@ -25,10 +25,19 @@ class Path(object):
             self.distance = np.array([])
             self.segments = 0
       def pathAssemble(self,ID, X,Y):
+            
+            for item in X:
+                  self.x.append(item) # check if this works for numpy arrays
+            for item in Y:
+                  self.y.append(item)
+            
+            
 
       def getParam(self, position):
+            pass
 
       def getPosition(self,param):
+            pass
 
 class SteeringOutput(object):
       def _init_(self):
@@ -79,6 +88,13 @@ def normalize(vector):
             return vector
       return vector / norm
 
+def distanceBetweenPoints(a, b):
+      math.sqrt(a)
+      pass
+
+def closestPointSegment(q, a, b):
+      pass
+
 def getSteeringSeek(Character, Target):
       # Create output structure
       result = SteeringOutput()
@@ -94,113 +110,6 @@ def getSteeringSeek(Character, Target):
 
       result.linear = Target.position - Character.position
 
-def getSteeringFlee(Character, Target):
-      # Create output structur
-      result = SteeringOutput()
-      #Get the direction to the target
-      result.linear = Character.position - Target.position
-      #result.linear = np.subtract(Character.position, Target.position)
-      # Accelerate at maximum rate
-      result.linear = normalize(result.linear)
-      result.linear *= Character.max_acceleration
-      # Output steering
-      result.angular = 0
-      return result
-      #result.angular = 0
-      return result
-      
-      
-
-def getSteeringArrive(Character, Target):
-
-      #if Character.position[0] == Target.position[0] and Character.position[1] == Target.position[1]:
-      #      Character.colcollide = True
-      
-      #if Character.colcollide:
-      #      return -Character.velocity
-
-      result = SteeringOutput()
-                  # Get the direction and distance to the target
-      direction = np.subtract(Target.position, Character.position) ## Check if these need to be switched later
-      distance = np.linalg.norm(direction) # Magnitude
-                  # Test for arrival
-      if distance < Character.arrival_radius:
-            #result.linear = 0
-            #result.angular
-            return result
-                   # Outside slowing-down (outer) radius, move at max speed
-      if distance > Character.slowing_radius: ### Not sure if this needs to be target or character slow radius
-            targetSpeed = Character.max_velocity
-      # Between radii, scale speed to slow down
-      else:
-            targetSpeed = Character.max_velocity * distance / Character.slowing_radius
-                  # Target velocity combines speed and direction
-      targetVelocity = direction
-      targetVelocity = normalize(targetVelocity)
-      targetVelocity *= targetSpeed
-                  # Accelerate to target velocity
-      result.linear = np.subtract(targetVelocity, Character.velocity)
-      result.linear /= Character.time_to_target
-                  # Test for too fast acceleratio
-      if np.linalg.norm(result.linear) > Character.max_acceleration:
-            result.linear = normalize(result.linear)
-            result.linear *= Character.max_acceleration
-
-      # Change might be wrong so commenting this out
-      # if result.linear.length() > maxAcceleration:
-      #      result.linear = normalize(result.linear)
-      #      result.linear *= maxAcceleration
-
-      # Output steering
-      #result.angular = 0
-      return result
-
-"""
-def getSteeringArrive(Character, Target):
-      result = SteeringOutput()
-                  # Get the direction and distance to the target
-      direction = np.subtract(Target.position, Character.position) ## Check if these need to be switched later
-      distance = np.linalg.norm(direction) # Magnitude
-                  # Test for arrival
-      if distance < targetRadius:
-            return None
-                   # Outside slowing-down (outer) radius, move at max speed
-      if distance > Target.slowing_radius: ### Not sure if this needs to be target or character slow radius
-            targetSpeed = maxSpeed
-      # Between radii, scale speed to slow down
-      else:
-            targetSpeed = maxSpeed * distance / Target.slowing_radius
-                  # Target velocity combines speed and direction
-      targetVelocity = direction
-      targetVelocity = normalize(targetVelocity)
-      targetVelocity *= targetSpeed
-                  # Accelerate to target velocity
-      result.linear = np.subtract(targetVelocity, Character.velocity)
-      result.linear /= timeToTarget
-                  # Test for too fast acceleratio
-      if np.linalg.norm(result.linear) > maxAcceleration:
-            result.linear = normalize(result.linear)
-            result.linear *= maxAcceleration
-
-      # Change might be wrong so commenting this out
-      # if result.linear.length() > maxAcceleration:
-      #      result.linear = normalize(result.linear)
-      #      result.linear *= maxAcceleration
-
-      # Output steering
-      #result.angular = 0
-      return result
-"""
-
-def getSteeringContinue(Character, Target):
-      #r code
-      #result < - list(linear=mover$linear, angular=mover$angular)
-      #return(result)
-      result = SteeringOutput
-      result.linear = Character.linear
-      result.angular = Character.orientation ### This may be angular acceleration, but for now we'll say orientation
-      
-      return result
 
 def testSteering(moveType):
       char1 = Character()
@@ -259,14 +168,14 @@ def main():
       characters[0].steer = 1
 
       # Initialize Character 2 FOLLOW
-      characters[1].id = 2701
-      characters[1].steer = 1
-      np.put(characters[1].position,[0,1],[20.0, 95])
-      np.put(characters[1].velocity,[0,1],[0,0])
-      characters[1].max_velocity = 4
-      characters[1].max_acceleration = 2
-      characters[1].path_to_follow = 1
-      characters[1].path_offset = 0.04
+      characters[0].id = 2701
+      characters[0].steer = 1
+      np.put(characters[0].position,[0,1],[20.0, 95])
+      np.put(characters[0].velocity,[0,1],[0,0])
+      characters[0].max_velocity = 4
+      characters[0].max_acceleration = 2
+      characters[0].path_to_follow = 1
+      characters[0].path_offset = 0.04
 
 
       time_step_length = 50
@@ -286,9 +195,5 @@ def main():
                               output = getSteeringArrive(character, character.target)
                   
                   movementUpdate(steering=output, time=0.5, character=character)
-            
-            
-            
-
       
 main()
